a6b1094526307671ca8b4aea8ad06b2bb96db724
Matt Horvat <mhorvaenator@gmail.com>
2018-07-23 20:12:32 +0200

    Styling

---
+++ src/app/you-tube-search/search-result.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { SearchResult } from './search-result.model';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: []
+})
+export class SearchResultComponent implements OnInit {
+
+  @Input() result: SearchResult;
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/you-tube-search/you-tube-search.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { SearchResult } from './search-result.model';
+
+@Component({
+  selector: 'app-you-tube-search',
+  templateUrl: './you-tube-search.component.html',
+  styleUrls: []
+})
+export class YouTubeSearchComponent implements OnInit {
+
+  results: SearchResult[];
+  loading: boolean;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  updateResults(results: SearchResult[]){
+    this.results = results;
+    // console.log("results:", this.results); // uncomment to take a look 
+  }
+
+}
