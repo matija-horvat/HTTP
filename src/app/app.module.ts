85bf031b9a39916fbf920eaead77afe4158c9c01
Matija Horvat <mhorvat@spin.hr>
2018-07-23 15:37:15 +0200

    Adding searchbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { SimpleHttpComponent } from './simple-http/simple-http.component';
+import { youTubeSearchInjectables } from './you-tube-search/you-tube-search.injectables';
+import { SearchBoxComponent } from './you-tube-search/search-box.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SimpleHttpComponent,
+    SearchBoxComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [ youTubeSearchInjectables ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/you-tube-search/you-tube-search.injectables.ts
@@ -0,0 +1,11 @@
+import{
+    YouTubeSearchService,
+    YOUTUBE_API_URL,
+    YOUTUBE_API_KEY
+} from './you-tube-search.service';
+
+export const youTubeSearchInjectables: Array<any>=[
+    {provide: YouTubeSearchService, useClass: YouTubeSearchService},
+    {provide: YOUTUBE_API_KEY, useValue: YOUTUBE_API_KEY},
+    {provide: YOUTUBE_API_URL, uyeValue: YOUTUBE_API_URL}
+];
